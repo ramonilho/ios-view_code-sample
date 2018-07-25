cabb222a8ce82038511f31cd91a175976d8a45dd
Ramon Honorio <ramon.honorio@miraeducacao.com.br>
2018-07-25 18:01:11 -0300

    Initial Commit

---
+++ ViewCodeSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewCodeSample
+//
+//  Created by Ramon Honório on 25/07/18.
+//  Copyright © 2018 Ramon Honório. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
